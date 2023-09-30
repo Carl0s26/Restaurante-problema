f1d5951d90c66c8cfa0200529ad2d1a5fb265a65
Carl0s26 <Carloskepp26@gmail.com>
2023-09-30 16:28:40 -0400

    fixed some errors

--- Restaurante Baratie/src/baratie.java
+++ Restaurante Baratie/src/baratie.java
@@ -1,67 +1,33 @@
+
 // Imports
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 // Starting Setup
 public class baratie {
-    /**
-     * @param args
-     * 
-     */
+    static Boolean isInTheReservationList(List<String> reservation_list, String user_name) {
+        for (String name : reservation_list) {
+            if (name.toLowerCase().equals(user_name.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-
-    public static void main(String[] args) {
-        Scanner axel = new Scanner(System.in);        
+    static void Restaurant(String user_name) {
+        Scanner axel = new Scanner(System.in);
         List<Choose> choose = new ArrayList<>();
-        List<Dessert> dessertsList = new ArrayList<>();
-        List<Menu> MenuList = new ArrayList<>();
-        dessertsList.add(new Dessert("Hamburgesa", 50));
-        dessertsList.add(new Dessert("Pescado", 70));
-        dessertsList.add(new Dessert("Sandwich_de_jamon", 50));
-        dessertsList.add(new Dessert("Lagsaña", 50));
-        dessertsList.add(new Dessert("Agua", 70));
-        dessertsList.add(new Dessert("Pizza", 50));
-        dessertsList.add(new Dessert("Cocacola", 50));
-        MenuList.add(new Menu("Icecream", 50));
-        MenuList.add(new Menu("Cake", 70));
-        MenuList.add(new Menu("Brownie", 20));
-        MenuList.add(new Menu("Cheescake", 20));
-        MenuList.add(new Menu("Flan", 15));
-        MenuList.add(new Menu("macarron", 50));
-        MenuList.add(new Menu("Mochi", 50));
-
-        
-        // Reservation List
-        
-             String[] reservation_list = { "Carlos", "Ethan", "Axel", "Jhon" };
-            System.out.println("What is your name?");
-            String user_name = axel.nextLine();
-
-            for (String name : reservation_list) {
-                if (name.equals(user_name)) {
-                    System.out.println("You are in the reservation list !!!");
-
-
-
-                    
-                {
-                    System.out.println("Thanks for your order" + user_name);
-                }
-        
-        
-
-        // menu display 
         int choice;
         do {
-       
-            System.out.println("#1 Hamburgesa");
-            System.out.println("#2 Pescado");
-            System.out.println("#3 Sandwich_de_jamon");
-            System.out.println("#4 Lagsaña");
-            System.out.println("#5 Agua");
-            System.out.println("#6 Pizza");
-            System.out.println("#7 Cocacola");
+            System.out.println("");
+            System.out.println("#1 hamburger: 40");
+            System.out.println("#2 Fish: 35");
+            System.out.println("#3 Sandwich: 25");
+            System.out.println("#4 Lagsaña: 40");
+            System.out.println("#5 Water: 5");
+            System.out.println("#6 Pizza: 20");
+            System.out.println("#7 Cocacola: 10");
             System.out.println("#8 Salida");
             System.out.println("");
             choice = axel.nextInt();
@@ -70,46 +36,44 @@ public class baratie {
                 default:
                     break;
                 case 1:
-                    choose.add(new Choose("Hamburgesa", 50));
+                    choose.add(new Choose("Hamburger", 40));
                     break;
                 case 2:
-                    choose.add(new Choose("Pescado", 70));
+                    choose.add(new Choose("Fish", 35));
                     break;
                 case 3:
-                    choose.add(new Choose("Sandwich_de_jamon", 50));
+                    choose.add(new Choose("Sandwich", 25));
                     break;
                 case 4:
-                    choose.add(new Choose("Lagsaña", 70));
+                    choose.add(new Choose("Lagsaña", 40));
                     break;
                 case 5:
-                    choose.add(new Choose("Agua", 50));
+                    choose.add(new Choose("Water", 5));
                     break;
                 case 6:
-                    choose.add(new Choose("Pizza", 70));
+                    choose.add(new Choose("Pizza", 20));
                     break;
                 case 7:
-                    choose.add(new Choose("Cocacola", 50));
+                    choose.add(new Choose("Cocacola", 10));
                     break;
                 case 8:
                     break;
             }
         } while (choice != 8);
-        
-        
 
         // dessert menu
-        
+
         System.out.println("Would you like a dessert?");
-        
+
         do {
-       
-            System.out.println("#1 Icecream");
-            System.out.println("#2 Cake");
-            System.out.println("#3 Brownie");
-            System.out.println("#4 Cheescake");
-            System.out.println("#5 Flan");
-            System.out.println("#6 macarron");
-            System.out.println("#7 Mochi");
+            System.out.println("");
+            System.out.println("#1 Icecream: 25");
+            System.out.println("#2 Cake: 50");
+            System.out.println("#3 Brownie: 20");
+            System.out.println("#4 Cheescake: 30");
+            System.out.println("#5 Flan: 25");
+            System.out.println("#6 macarron: 15");
+            System.out.println("#7 Mochi: 10");
             System.out.println("#8 Salida");
             System.out.println("");
             choice = axel.nextInt();
@@ -118,32 +82,30 @@ public class baratie {
                 default:
                     break;
                 case 1:
-                    choose.add(new Choose("Icecream", 50));
+                    choose.add(new Choose("Icecream: ", 25));
                     break;
                 case 2:
-                    choose.add(new Choose("Cake", 70));
+                    choose.add(new Choose("Cake:", 50));
                     break;
                 case 3:
-                    choose.add(new Choose("Brownie", 50));
+                    choose.add(new Choose("Brownie:", 20));
                     break;
                 case 4:
-                    choose.add(new Choose("Cheescake", 70));
+                    choose.add(new Choose("Cheescake:", 30));
                     break;
                 case 5:
-                    choose.add(new Choose("Flan", 50));
+                    choose.add(new Choose("Flan:", 25));
                     break;
                 case 6:
-                    choose.add(new Choose("macarron", 70));
+                    choose.add(new Choose("macarron:", 15));
                     break;
                 case 7:
-                    choose.add(new Choose("Mochi", 50));
+                    choose.add(new Choose("Mochi:", 10));
                     break;
                 case 8:
                     break;
             }
         } while (choice != 8);
-
-        
         System.out.println("");
         System.out.println("Your total order is:");
         System.out.println("");
@@ -152,11 +114,38 @@ public class baratie {
             System.out.println(choose.get(i).getName() + " " + choose.get(i).getPrice());
             sum += choose.get(i).getPrice();
         }
-        double sum_percentage = sum / 100; 
-        double sum_taxed = sum_percentage * 0.28;
-        System.out.println("Total: " + sum_taxed );
-
+        double sum_total = ((sum * 0.28) + sum);
+        System.out.println("Total without taxes " + sum);
+        System.out.println("Total: " + sum_total);
+        System.out.println("");
+        System.out.println("Have a good day " + user_name);
+        axel.close();
     }
+
+    public static void main(String[] args) {
+        List<String> reservation_list = new ArrayList<>();
+        reservation_list.add("Carlos");
+        reservation_list.add("Ethan");
+        reservation_list.add("Axel");
+        reservation_list.add("Jhon");
+        String user_input;
+        // Reservation List
+        Scanner axel = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String user_name = axel.nextLine();
+        if (isInTheReservationList(reservation_list, user_name).equals(true)) {
+            Restaurant(user_name);
+        } else {
+            System.out.println("you aren't in the reservation list");
+            System.out.println("Would you like to make a reservation? yes/no");
+            user_input = axel.nextLine();
+            if (user_input.equals("yes")) {
+                reservation_list.add(user_name);
+                Restaurant(user_name);
+            } else {
+                System.out.println("Then go away");
             }
         }
+        axel.close();
     }
+}
